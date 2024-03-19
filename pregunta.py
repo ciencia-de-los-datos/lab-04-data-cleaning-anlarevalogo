0dcb8d8fd6f3eed03badd3c662bb09316a1fabae
anlarevalogo <160282139+anlarevalogo@users.noreply.github.com>
2024-03-18 21:00:30 -0500

    Add files via upload

--- pregunta.py
+++ pregunta.py
@@ -13,8 +13,25 @@ def clean_data():
 
     df = pd.read_csv("solicitudes_credito.csv", sep=";")
 
-    #
-    # Inserte su código aquí
-    #
+    # Limpiar los datos
+    # Convertir todas las cadenas a minúsculas
+    df = df.apply(lambda x: x.astype(str).str.lower())
+
+    # Convertir fechas al formato correcto
+    df['fecha_de_beneficio'] = pd.to_datetime(df['fecha_de_beneficio'], errors='coerce')
+
+    # Remover caracteres especiales del monto del crédito y convertirlo a numérico
+    df['monto_del_credito'] = df['monto_del_credito'].str.replace('[^0-9]', '', regex=True).astype(float)
+
+    # Eliminar filas con datos faltantes
+    #df = df.dropna()
+
+    # Eliminar valores duplicados
+    df = df.drop_duplicates()
+
+    # Guardar el DataFrame limpio como un archivo CSV
+    df.to_csv('solicitudes_credito_clean.csv', index=False)
 
     return df
+
+clean_data()
